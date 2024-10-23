699804164ffed8a497eea6b1deb12fd883bb0392
Valeriehavey-ga <valeriehavey@gmail.com>
2024-10-23 12:58:19 -0700

    Setup through Nav partial

--- models/user.js
+++ models/user.js
@@ -1,5 +1,24 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  foodName: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  foodType: {
+    type: String,
+    enum: ['canned', 'jarred', 'refrigerated item', 'frozen item', 'fresh vegetable', 'fresh fruit', 'snack item', 'seasoning', 'staple'],
+    required: true,
+  },
+  notes: {
+    type: String,
+  }
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +28,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema],
 });
 
 const User = mongoose.model('User', userSchema);
